4a0c981d1af83d44a21cba50b787a84a3f2da62d
kknoerz <kevink430@gmail.com>
2016-03-22 09:17:47 -0400

    fixed scissors block

--- assets/javascript/ap.js
+++ assets/javascript/ap.js
@@ -3,17 +3,19 @@
 var firebase = new Firebase("https://kknoerz.firebaseio.com/");
 var playerName = ''
 var opponent = ''
-var playerBox = []
-var addPlayer = {}
 var wins = 0
 var losses = 0
 var tie = 0
 var youChosen = ""
 var oppChosen = ""
+var youCoinFlip
+var oppCoinFlip
+
+
 
 //*********** If You Are Seccond To Arrive ****************************
 firebase.on('child_added', function(snapshot) {
-	debugger;
+	// debugger;
 	
 	if (snapshot.val().you != playerName && snapshot.val().you != undefined && snapshot.val().player != opponent) { //you are seccond to arrive
 		opponent = snapshot.val().you
@@ -29,11 +31,6 @@ firebase.on('child_added', function(snapshot) {
 			'opacity': '0.2',
 			'background-color': 'red'
 		});
-
-		
-
-
-
 
 // ********** If You Are First To Arrive ********************************
 	}else if (snapshot.val().you = playerName && snapshot.val().you != undefined) { 
@@ -52,36 +49,50 @@ firebase.on('child_added', function(snapshot) {
 
 		if ((youChosen == 'rock') && (oppChosen == 'scissors')){
             wins++;
+            youChosen = ''
+            oppChosen = ''
             $('#smackRead').prepend('<br />').prepend($('<label>').text('You won! you have: '+wins+' wins!'));
         
         
         }else if ((youChosen == 'rock') && (oppChosen == 'paper')){
             losses++;
+            youChosen = ''
+            oppChosen = ''            
             $('#smackRead').prepend('<br />').prepend($('<label>').text('You lost! you have: '+losses+' losses!'));            
         
         
         }else if ((youChosen == 'scissors') && (oppChosen == 'rock')){
             losses++;
+            youChosen = ''
+            oppChosen = ''            
 			$('#smackRead').prepend('<br />').prepend($('<label>').text('You lost! you have: '+losses+' losses!'));            
         
         
         }else if ((youChosen == 'scissors') && (oppChosen == 'paper')){
             wins++;
+            youChosen = ''
+            oppChosen = ''            
             $('#smackRead').prepend('<br />').prepend($('<label>').text('You won! you have: '+wins+' wins!'));            
         
 
         }else if ((youChosen == 'paper') && (oppChosen == 'rock')){
             wins++;
+            youChosen = ''
+            oppChosen = ''            
             $('#smackRead').prepend('<br />').prepend($('<label>').text('You won! you have: '+wins+' wins!'));            
         
 
         }else if ((youChosen == 'paper') && (oppChosen == 'scissors')){
             losses++;
+            youChosen = ''
+            oppChosen = ''            
             $('#smackRead').prepend('<br />').prepend($('<label>').text('You lost! you have: '+losses+' losses!'));
         
 
         }else if (youChosen == oppChosen){
             tie++;
+            youChosen = ''
+            oppChosen = ''            
             $('#smackRead').prepend('<br />').prepend($('<label>').text('You tied! you have: '+tie+' ties!'));
         } 
 	}
@@ -91,7 +102,7 @@ firebase.on('child_added', function(snapshot) {
 //************* Rock Paper Scissors Selection *******************************
 
 $(document).on('click', '#playerOneChoose .rock', function(){
-	debugger;
+	// debugger;
 	if (playerName != ''){
 		youChosen = 'rock'
 		$('#smackRead').prepend('<br />').prepend($('<label>').text('You chose '+youChosen));		
@@ -104,7 +115,7 @@ $(document).on('click', '#playerOneChoose .rock', function(){
 });
 
 $(document).on('click', '#playerOneChoose .paper', function(){
-	debugger;
+	// debugger;
 	if (playerName != ''){
 		youChosen = 'paper'
 		$('#smackRead').prepend('<br />').prepend($('<label>').text('You chose paper'));
@@ -117,12 +128,13 @@ $(document).on('click', '#playerOneChoose .paper', function(){
 });
 
 $(document).on('click', '#playerOneChoose .scissors', function(){
-	debugger;
+	// debugger;
 	if (playerName != ''){
-		youChosen: "scissors"
+		youChosen = "scissors"
 		$('#smackRead').prepend('<br />').prepend($('<label>').text('You chose scissors'));	
 		var scissors = {
 			player: playerName,
+			chose: 'scissors'
 
 		}
 		firebase.push(scissors)		
@@ -146,7 +158,15 @@ $(document).on('click', '#playerOneChoose .scissors', function(){
 
 // });
 
+//***************** Coin Flip Logic **************************************
 
+var flip = function() {
+	youChosen = Math.random();
+	console.log(youChosen);
+	
+	oppChosen = Math.random();
+	console.log(oppChosen);
+}
 
 
 // When player name is entered ********************************************
@@ -164,11 +184,12 @@ $('#submitName').on('click', function() {
 
 	$('#player1').append(playerOneChoose);
 	$('#playerOneChoose').append(rock, paper, scissors);
+
 	$('#playerName').remove();
 	$('#submitName').remove()
 	$('#player-input').remove()
 	$('body').prepend($('<div id="player-input">').text('Select one!'))	
-	debugger;
+	// debugger;
 
 	user = {
 		you: playerName 
